c9798ccc199ba2124dd49d51f7df8dfd34d37aef
alicendeh <alicendeh16@gmail.com>
2022-01-14 17:55:21 +0100

    Done with creating my dummy dataset

--- src/BottomNavigation/BottomNavigation.js
+++ src/BottomNavigation/BottomNavigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { Favorites, AllRecipes } from "../screens";
 
